dda78dfb8837a2f636abb785b3785678e909f21b
Himanshu Vishwakarma <himvish997@gmail.com>
2019-06-02 19:22:55 +0530

    Update the agrsparser message in main.py

--- LeNet-5/download_data.py
+++ LeNet-5/download_data.py
@@ -6,22 +6,24 @@ size 28*28*1. We need to resize it to 32*32*1 for the LeNet-5 Model.'''
 
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot = True)
 
 
 def train_data():
+    mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot = True)
     X_train, y_train = mnist.train.images, mnist.train.labels
     X_train = np.pad(X_train, ((0,0), (2,2), (2,2), (0,0)), 'constant')
     return X_train, y_train
 
 
 def val_data():
+    mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot = True)
     X_val, y_val = mnist.validation.images, mnist.validation.labels
     X_val =   np.pad(X_val, ((0,0), (2,2), (2,2), (0,0)), 'constant')
     return X_val, y_val
 
 
 def test_data():
+    mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot = True)
     X_test, y_test = mnist.test.images, mnist.test.labels
     X_test =  np.pad(X_test, ((0,0), (2,2), (2,2), (0,0)), 'constant')
     return X_test, y_test
--- LeNet-5/main.py
+++ LeNet-5/main.py
@@ -10,13 +10,12 @@ from ReInitializable_iterator import ReInitializable_iterator
 from Feedable_iterator import Feedable_iterator
 from download_data import train_data, val_data, test_data
 
-
-parser = argparse.ArgumentParser(description='Running the LeNet-5 model with the variours Iterator in the tensorflow')
-parser.add_argument("-osi", "--one_shot_iterator", help="Running by the One Shot Iterator", action='store_true')
-parser.add_argument("-ii", "--initializable_iterator", help="Running by the initializable iterator",
+parser = argparse.ArgumentParser(description='Training the LeNet-5 model with the variours data pipeline method (Iterator) in the tensorflow')
+parser.add_argument("-osi", "--one_shot_iterator", help="One-shot iterator: The Dataset can’t be reinitialized once exhausted. To train for more epochs, you would need to repeat the Dataset before feeding to the iterator. This will require huge memory if the size of the data is large. It also doesn’t provide any option to validate the model.", action='store_true')
+parser.add_argument("-ii", "--initializable_iterator", help="Initializable iterator: You can dynamically change the Dataset between training and validation Datasets. However, in this case both the Datasets needs to go through the same transformation pipeline.",
                     action='store_true')
-parser.add_argument("-ri", "--reinitializable_iterator", help="Running by the Re-initializable iterator", action='store_true')
-parser.add_argument("-fi", "--feedable_iterator", help="Running by the Feedable iterator", action='store_true')
+parser.add_argument("-ri", "--reinitializable_iterator", help="Re-initializable iterator: This iterator overcomes the problem of initializable iterator by using two separate Datasets. Each dataset can go through its own preprocessing pipeline. The iterator can be created using the tf.Iterator.from_structure method.", action='store_true')
+parser.add_argument("-fi", "--feedable_iterator", help="Feedable iterator: This iterator provides the option of switching between various iterators. You can create a re-initializable iterator for training and validation purposes. For inference/testing where you require one pass of the dataset, you can use the one shot iterator.", action='store_true')
 args = parser.parse_args()
 
 def main():
@@ -25,23 +24,21 @@ def main():
     if args.one_shot_iterator:
         X_train, Y_train = train_data()
         One_Shot_iterator(X_train, Y_train)
-    if args.initializable_iterator:
+    elif args.initializable_iterator:
         X_train, Y_train = train_data()
         X_val, Y_val = val_data()
         initializable_iterator(X_train, Y_train, X_val, Y_val)
-    if args.reinitializable_iterator:
+    elif args.reinitializable_iterator:
         X_train, Y_train = train_data()
         X_val, Y_val = val_data()
         ReInitializable_iterator(X_train, Y_train, X_val, Y_val)
-    if args.feedable_iterator:
+    elif args.feedable_iterator:
         X_train, Y_train = train_data()
         X_val, Y_val = val_data()
         X_test, Y_test = test_data()
         Feedable_iterator(X_train, Y_train, X_val, Y_val, X_test, Y_test)
-
-
-
-
+    else:
+        print("Please give some argparse. For more try:\n    python3 main.py -h or --help")
 
 
 if __name__ == '__main__':
